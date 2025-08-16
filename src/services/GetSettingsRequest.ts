13ab6039713a42fecabd63e10483927522ed1990
ehabyanni <ehabayanni@gmail.com>
2025-08-16 13:04:06 +0300

    Reduces cache duration for testing purposes.

--- src/services/GetSettingsRequest.ts
+++ src/services/GetSettingsRequest.ts
@@ -6,7 +6,7 @@ export const getSettings = cache(async () => {
   try {
     const data: SettingsResponse = await api.static<SettingsResponse>(
       "home",
-      3600
+      10 //shuold be 60 minutes, but we use 10 for testing purposes
     ); //extend the cache time to 1 hour to increase the speed of the app
     return data?.data as Settings;
   } catch (error) {
