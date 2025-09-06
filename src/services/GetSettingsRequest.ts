e478b2815ab74d16faab5cd22eac30edea3843e5
ehabyanni <ehabayanni@gmail.com>
2025-09-06 12:15:36 +0300

    Increase cache time to 6 hrs

--- src/services/GetSettingsRequest.ts
+++ src/services/GetSettingsRequest.ts
@@ -6,8 +6,8 @@ export const getSettings = cache(async () => {
   try {
     const data: SettingsResponse = await api.static<SettingsResponse>(
       "home",
-      10 //shuold be 60 minutes, but we use 10 for testing purposes
-    ); //extend the cache time to 1 hour to increase the speed of the app
+      60 * 60 * 6
+    );
     return data?.data as Settings;
   } catch (error) {
     console.log(error);
