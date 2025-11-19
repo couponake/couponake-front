1c18f342f62fc1e56d51917b5235ee4a334386ba
ehabyanni <ehabayanni@gmail.com>
2025-11-19 13:17:22 +0200

    Disable Cloudflare Rocket Loader on that script only It is an attribute used specifically for Cloudflare’s Rocket Loader. Rocket Loader tries to speed up page loads by delaying all JavaScript until the page finishes rendering.

--- src/components/Pages/ContactUsPage.tsx
+++ src/components/Pages/ContactUsPage.tsx
@@ -131,6 +131,9 @@ const ContactUsPage = () => {
 
   return (
     <>
+      <head>
+        <script data-cfasync="false"></script>
+      </head>
       <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white -mt-5">
         <div className="container mx-auto px-4 py-16">
           <div className="text-center mb-12">
