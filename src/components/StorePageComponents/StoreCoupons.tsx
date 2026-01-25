8e7c24efbf7e4ba422fe23c49ff5a9f1d0e58c07
ehabyanni <ehabayanni@gmail.com>
2026-01-25 19:31:12 +0200

    Remove the Coupons pagination code and render all the whole list f the store coupons.

--- src/components/StorePageComponents/StoreCoupons.tsx
+++ src/components/StorePageComponents/StoreCoupons.tsx
@@ -1,9 +1,7 @@
 "use client";
 import StoreCoupon from "@/components/StorePageComponents/StoreCoupon";
 import { CouponProps } from "@/types";
-import { Button } from "@heroui/button";
-import { useTranslations } from "next-intl";
-import React, { useState } from "react";
+import React from "react";
 
 interface StoreCouponsProps {
   store_coupons: CouponProps[];
@@ -11,54 +9,20 @@ interface StoreCouponsProps {
 }
 
 function StoreCoupons({ store_coupons, store_image }: StoreCouponsProps) {
-  const t = useTranslations();
-  const [visibleCount, setVisibleCount] = useState<number>(3);
 
-  const handleShowMore = () => {
-    setVisibleCount((prev) => Math.min(prev + 3, store_coupons?.length || 0));
-  };
-  const handleShowLess = () => {
-    setVisibleCount(3);
-  };
-
-  const couponsToShow = store_coupons?.slice(0, visibleCount);
-  const allVisible = visibleCount >= (store_coupons?.length || 0);
+  const couponsToShow = store_coupons?.slice(0, store_coupons?.length || 0);
 
   return (
-    <div className="space-y-3 bg-red-400/0">
+    <div className="space-y-3">
       {store_coupons && store_coupons.length > 0 && (
-        <>
-          {couponsToShow &&
+          couponsToShow &&
             couponsToShow.map((coupon: CouponProps) => (
               <StoreCoupon
                 key={coupon?.id}
                 coupon={coupon}
                 store_image={store_image}
               />
-            ))}
-
-          <div className="pt-5">
-            {!allVisible ? (
-              <Button
-                variant="light"
-                color="primary"
-                className="w-full border-1 border-main-200"
-                onPress={handleShowMore}
-              >
-                {t("More Coupons")}
-              </Button>
-            ) : (
-              <Button
-                variant="light"
-                color="primary"
-                className="w-full border-1 border-main-200"
-                onPress={handleShowLess}
-              >
-                {t("Less Coupons")}
-              </Button>
-            )}
-          </div>
-        </>
+            ))
       )}
     </div>
   );
