13413858a0ec43856ca3b0587311f8c360bdce62
ehabyanni <ehabayanni@gmail.com>
2026-02-12 01:50:50 +0200

    Remove the FAQs title bg-color

--- src/components/Pages/Home/FAQ.tsx
+++ src/components/Pages/Home/FAQ.tsx
@@ -30,7 +30,7 @@ const FAQ = ({
   return (
     <section className={cn("container py-8 md:py-11 lg:py-20", className)}>
       <div className="flex gap-5 flex-wrap items-start justify-between my-5">
-        <h3 className="text-lg md:text-2xl font-semibold text-neutral-900 sm:text-xl bg-red-500">
+        <h3 className="text-lg md:text-2xl font-semibold text-neutral-900 sm:text-xl">
           {title}
         </h3>
       </div>
