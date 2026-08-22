e56566827887595b907870c5192171041317b7d1
ehabyanni <ehabayanni@gmail.com>
2026-08-22 16:47:43 +0300

    ref(footer links): Remove tne importantStoresLinks

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -58,7 +58,7 @@ const Footer = ({
       className="mt-auto bg-background text-foreground"
     >
       <div className="mx-auto px-4 py-12 mb-12 lg:mb-0">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 container">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 container">
           <div className="space-y-4">
             <Link target="_self" href="/" aria-label="Home" prefetch={false}>
               {socialLinks.logo && (
